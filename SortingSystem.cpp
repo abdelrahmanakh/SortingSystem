b0a76588019a874f145d97d56f4def5af20075f8
Abdelrahman Khateeb <akhteebabdo@gmail.com>
2025-04-03 18:16:30 +0200

    Menu Function Added

--- SortingSystem.cpp
+++ SortingSystem.cpp
@@ -87,6 +87,78 @@ void SortingSystem<T>::measureSortTime(void (SortingSystem::*sortFunc)()) {
 
 template <typename T>
 void SortingSystem<T>::showMenu() {
-    // TODO: Implement user interaction menu
-}
+    while (true) {
+        int n;
+        cout << "Enter the number of items to sort:";
+        cin >> n;
+        string data_test[n];
+        for (int i = 0; i < n; i++) {
+            cout << "Enter data " << i + 1 << ":";
+            cin >> data_test[i];
+        }
+        int choice;
+        cout<<"Select a sorting algorithm:\n";
+        cout<<"1. Insertion Sort\n";
+        cout<<"2. Selection Sort\n";
+        cout<<"3. Bubble Sort\n";
+        cout<<"4. Shell Sort\n";
+        cout<<"5. Merge Sort\n";
+        cout<<"6. Quick Sort\n";
+        cout<<"7. Count Sort\n";
+        cout<<"8. Radix Sort\n";
+        cout<<"9. Bucket Sort\n";
+        cout<<"Enter your choice (1-9):";
+        while (true) {
+            cin>>choice;
+            switch (choice) {
+                case 1:
+                    //insertionSort();
+                        break;
+                case 2:
+                    //selectionSort();
+                        break;
+                case 3:
+                    //bubbleSort();
+                        break;
+                case 4:
+                    //shellSort();
+                        break;
+                case 5:
+                    //mergeSort();
+                        break;
+                case 6:
+                    //quickSort();
+                        break;
+                case 7:
+                    //countSort();
+                        break;
+                case 8:
+                    //radixSort();
+                        break;
+                case 9:
+                    //bucketSort();
+                        break;
+                default:
+                    cout<<"Wrong choice, Please Enter your choice (1-9):";
+                continue; //wrong input so cin again
+            }
+            break;
+        }
+        char choice2;
+        cout<<"Do you want sort another dataset? (y/n):";
+        while (true) {
+            cin>>choice2;
+            switch (choice2) {
+                case 'y':
+                    break;
+                case 'n':
+                    cout<<"Thank you for using the sorting system! Goodbye!";
+                    return;
+                default:
+                    cout<<"Wrong choice, Please Enter your choice (y/n):";
+                    continue;
+            }
+            break;
+        }
+    }}
 
